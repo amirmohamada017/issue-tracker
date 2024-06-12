31a5a87d4321c4d80466e334873a2293fb46c3c0
Amirmohamad <amirmohamada017@gmail.com>
2024-06-13 03:06:27 +0330

    Add sign in and sign out links to navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,16 +1,21 @@
 "use client";
 
 import classNames from "classnames";
+import { useUser, SignOutButton, SignInButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
+import { Box } from "@radix-ui/themes";
 
 const NavBar = () => {
   const currentPath = usePathname();
+  const { isLoaded, isSignedIn, user } = useUser();
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
@@ -18,19 +23,21 @@ const NavBar = () => {
       </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
-          <Link
-            key={link.href}
-            href={link.href}
-            className={classNames({
-              "text-zinc-900": link.href === currentPath,
-              "text-zinc-500": link.href !== currentPath,
-              "hover:text-zinc-900 transition-colors": true,
-            })}
-          >
-            {link.label}
-          </Link>
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className={classNames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-900 transition-colors": true,
+              })}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
+      <Box>{isLoaded && isSignedIn ? <SignOutButton /> : <SignInButton />}</Box>
     </nav>
   );
 };
